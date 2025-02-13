0a97467278680d7bc113b36bdc6686875b6285a8
Rafael Tavares <tavares.rfl@gmail.com>
2025-02-13 00:07:34 -0300

    Database seeding

--- src/main/java/com/group/crud_client/entities/Client.java
+++ src/main/java/com/group/crud_client/entities/Client.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_client")
 public class Client {
 
     @Id
---
+++ src/main/resources/import.sql
@@ -0,0 +1,19 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Roger Federer', '84606459245', 250000.0, '1981-08-08', 4);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Serena Williams', '85482406712', 180000.0, '1981-09-26', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gustavo Kuerten', '84562065722', 130000.0, '1976-09-10', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rafael Nadal', '80054856126', 240000.0, '1986-06-03', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria Sharapova', '83789932012', 200000.0, '1987-04-19', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Anna Kournikova', '89872351003', 120000.0, '1981-06-07', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Novak Djokovic', '85600932885', 270000.0, '1987-05-22', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Grigor Dimitrov', '80123588445', 159000.0, '1991-05-16', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Andy Murray', '83449200546', 120000.0, '1987-05-15', 4);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Fonseca', '80453567102', 50000.0, '2006-08-21', 0);
